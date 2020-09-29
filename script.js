2f951089d7219397e192b0a865ab60cc79bc6a52
Carlos <65001161+Tklao@users.noreply.github.com>
2020-09-29 14:26:45 -0300

    Up🎉🎉🎉 Up

---
+++ script.js
@@ -0,0 +1,8 @@
+//Função para validar email
+function clicar() {
+    var txt = document.querySelector('.caixa_txt')
+    if (txt.value == "") {
+        alert('Por favor, preencha o campo corretamente.')
+    }
+}
+
